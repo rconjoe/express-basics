880584662a5712fb7e18c4b4ad5375d75dac1691
nahabster <nahabed.alex@gmail.com>
2022-09-02 14:38:10 -0700

    Created alex.js file

---
+++ alex.js
@@ -0,0 +1,30 @@
+const app = require("express")();
+
+const { join } = require("path");
+
+function myMiddleware(req, res, next) {
+  const date = new Date().toJSON();
+  console.log(`request for route "${req.path}" at` + date);
+
+  next();
+}
+app.use(myMiddleware);
+
+function onHomeGetRequest(req, res) {
+  if (req.query && req.query.user) {
+    console.log(`user ${req.query.user} has landed`);
+  }
+  res.sendFile(join(__dirname, "public", "index.html"));
+}
+
+app.get("/", onHomeGetRequest);
+
+app.get("/pickles", (req, res) => {
+  res.json({
+    message: "Welcome to Pickle Page",
+  });
+});
+
+// where is favicon coming from
+// how to create different endpoints ex localhost:3004/api/proudcts
+app.listen(3004, () => console.log("listening on 3004"));
